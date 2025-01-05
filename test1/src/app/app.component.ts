7b70dfcc589ca18155003cf04aebe108e2aaf1c9
Risto Kizov <131457520+shandarovska@users.noreply.github.com>
2025-01-05 22:20:54 +0100

    Domasna 6

--- test1/src/app/app.component.ts
+++ test1/src/app/app.component.ts
@@ -1,24 +1,32 @@
+// app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CompComponent } from "./comp/comp.component";
 import { DRIVERS } from '../db-data';
 import { CommonModule } from '@angular/common';
+import { driver } from '../driver';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, CompComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test';
+  title = 'MotoGP Riders';
 
-  PB = DRIVERS[0];
-  JM = DRIVERS[1];
-  MM = DRIVERS[2];
-  vozaci = DRIVERS;
+  // Individual driver references
+  pecco: driver = DRIVERS[0];
+  martin: driver = DRIVERS[1];
+  marquez: driver = DRIVERS[2];
   
-  onAppView(){  
-  console.log("app e funkcijata.")
+  // Full drivers array
+  vozaci = DRIVERS;
+
+  kliknatVozac() {
+    console.log("App component function called");
   }
+
+
 }
--- test1/src/app/comp/comp.component.ts
+++ test1/src/app/comp/comp.component.ts
@@ -1,19 +1,35 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { driver } from '../../driver';
 
 @Component({
   selector: 'app-comp',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './comp.component.html',
   styleUrl: './comp.component.css'
 })
-export class CompComponent { //ts
-@Input()
-vozac: driver | undefined;
-@Input()
-indeks: Number | undefined;
+export class CompComponent {
+  @Input() vozac: driver | undefined;
+  @Input() indeks: number | undefined;
 
-onDrvView(event: Event) {
-  console.log("Me klikna");
-}
+  stilIme() {
+    return {
+      'name-display': true,
+      'special-driver': this.vozac?.category === 'EXPERT'
+    };
+  }
+
+  stilKarticka() {
+    return {
+      'driver-card': true,
+      'expert': this.vozac?.category === 'EXPERT',
+      'lunatic': this.vozac?.category === 'LUNATIC',
+      'beginner': this.vozac?.category === 'ASD'
+    };
+  }
+
+  kliknatKopce() {
+    console.log('Driver details requested for:', this.vozac?.name);
+  }
 }
